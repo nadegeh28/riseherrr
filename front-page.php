b5f932deb9376c941e3792eda1af9db99095b5b5
nadegeh28 <147403183+nadegeh28@users.noreply.github.com>
2024-12-06 16:07:56 +0100

    page d'accueil

--- front-page.php
+++ front-page.php
@@ -1,8 +1,29 @@
 <?php get_header(); ?>
 
-<main>
-    <h1>Bienvenue sur la page d'accueil</h1>
-    <p>Contenu de la page d'accueil...</p>
-</main>
+<section class="welcome-section">
+  <div class="welcome-container">
+    <!-- Image circulaire -->
+    <div class="welcome-image">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img1.jpg" alt="image1">
+    </div>
+    <!-- Texte -->
+    <div class="welcome-text">
+      <h2>
+         Bienvenue sur Rise Her : un espace où chaque Rose retrouve soutien, écoute et espoir.
+       
+      </h2>
+      <p>
+        Chaque femme mérite de se sentir en sécurité, écoutée et respectée. Ici, nous vous offrons un espace pour guérir, partager et reprendre le contrôle de votre vie. Ensemble, nous bâtissons un avenir où vous pouvez vous épanouir librement.
+      </p>
+      <br><br>
+      <a href="qui-sommes-nous.php" class="welcome-link">
+        <?php echo "Qui sommes nous ?"; ?>
+      </a>
+    </div>
+  </div>
+</section>
+
+
+
 
 <?php get_footer(); ?>
