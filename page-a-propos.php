422ba38f8469e404f979afd68dda3d3b40f69318
nadegeh28 <147403183+nadegeh28@users.noreply.github.com>
2024-12-08 14:02:50 +0100

    ajout contenu

--- page-a-propos.php
+++ page-a-propos.php
@@ -4,8 +4,99 @@
 
 
 
+<div class="container">
+        <div class="nous-container">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/imgnous.jpg" alt="img">
+        </div>
+        <div class="nous-text">
+            <h1>Qui sommes-nous ?</h1>
+            <p>
+                Chez Rise Her, nous vous appelons « Roses », parce que vous partagez 
+                avec ces fleurs une force et une résilience uniques. Comme les roses, 
+                vous êtes belles et fortes, même après les tempêtes.
+            </p>
+            <p>
+                Le mot « Rise » symbolise votre capacité à vous relever, à vous 
+                reconstruire et à retrouver votre pouvoir après les épreuves. Nous 
+                croyons en votre force de renaissance et en votre droit de vivre en paix, 
+                avec respect et dignité.
+            </p>
+            <p>
+                En tant que Roses, vous faites partie d'une communauté de soutien, 
+                une de l'art de la compréhension et la compassion. Ici, chaque voix est 
+                entendue et votre expérience est accueillie chaleureusement pour partager 
+                votre histoire en toute confiance, sans jugement.
+            </p>
+            <p>
+                Nous sommes là pour vous écouter, vous accompagner et vous offrir 
+                les ressources et le soutien nécessaires pour que vous puissiez, pas à pas, 
+                retrouver votre liberté et votre sérénité.
+            </p>
+        </div>
+    </div>
 
+    <div class="content">
+    <section>
+        <h5>L’origine de Rise Her</h5>
+        <p>
+            Rise Her est né du besoin de créer un espace sûr et bienveillant pour les femmes victimes de violences 
+            psychologiques et physiques. Beaucoup de femmes souffrent en silence, souvent sans savoir où chercher du soutien, 
+            et sont confrontées à des violences invisibles comme la manipulation, l’isolement et la dévalorisation. Ce site a été 
+            fondé pour leur offrir un refuge où elles peuvent se sentir entendues, comprises et soutenues dans leur parcours de guérison.
+        </p>
+        <p>
+            Inspirée par l’accompagnement de nombreuses femmes confrontées à ces abus, l’équipe de Rise Her croit fermement en 
+            la résilience et la reconstruction. Ce projet vise non seulement à offrir des ressources pratiques et des conseils, 
+            mais aussi à créer une communauté solidaire, où chaque femme peut partager son histoire sans jugement et retrouver sa force. 
+            Rise Her est un espace de soutien, de solidarité et de renaissance, où chaque femme est invitée à se relever et à reprendre 
+            le contrôle de sa vie.
+        </p>
+    </section>
 
+    <section>
+        <h5>Nos valeurs</h5>
+        <p>
+            Chez Rise Her, nous plaçons l’anonymat, la bienveillance et l’entraide au cœur de notre mission. Nous offrons un espace sûr 
+            et confidentiel où chaque femme peut s’exprimer librement, sans crainte d’être jugée, et où sa vie privée est étroitement respectée. 
+            La bienveillance est primordiale : chaque membre de notre communauté est accueilli avec respect, écoute et compassion, afin de lui 
+            fournir un soutien véritable et sans condition dans son processus de guérison. Enfin, nous croyons profondément en l’entraide.
+        </p>
+        <p>
+            En créant un réseau solidaire, nous permettons à chaque femme de se soutenir mutuellement, d’échanger des expériences et de 
+            partager des ressources, pour avancer ensemble sur le chemin de la reconstruction et de la résilience.
+        </p>
+    </section>
+
+    <section>
+        <h5>Nos services</h5>
+        <p>
+            Chez Rise Her, nous proposons une série de services conçus pour accompagner les femmes victimes de violences psychologiques 
+            et physiques. Notre formulaire d’auto-évaluation permet aux femmes de mieux comprendre si elles sont victimes de violences 
+            psychologiques en répondant à des questions précises.
+        </p>
+        <p>
+            Le blog offre des articles, des témoignages et des conseils pratiques pour aider à comprendre et à surmonter les violences. 
+            Le forum est un espace d’échange et de soutien où les femmes peuvent partager leurs expériences et trouver des ressources collectives. 
+            Nous mettons également à disposition des numéros de contacts d’urgence pour une aide immédiate. Enfin, notre équipe de spécialistes, 
+            composée d’un psychologue et d’une avocate, propose un accompagnement personnalisé pour aider à traiter les aspects émotionnels 
+            et juridiques des situations difficiles.
+        </p>
+    </section>
+
+    <section>
+        <h5>L’importance de la communauté</h5>
+        <p>
+            Chez Rise Her, l’entraide au sein de notre communauté est essentielle pour soutenir les femmes victimes de violences psychologiques 
+            et physiques. Nous croyons fermement que se sentir comprise et entourée d’autres femmes qui vivent des situations similaires peut 
+            être un levier puissant dans le processus de guérison. L’isolement amplifie la souffrance, mais l’entraide permet de briser ce cycle.
+        </p>
+        <p>
+            En partageant leurs expériences, les femmes trouvent un espace de soutien mutuel, de réconfort et de renouveau. Cette solidarité crée 
+            un environnement propre à la reconstruction, où chacune peut avancer à son rythme, se renforcer et se reconstruire en toute sécurité, 
+            en sachant qu’elle n’est jamais seule.
+        </p>
+    </section>
+</div>
 
 </div>
 <?php get_footer(); ?>
