3722011905135d9c056d47189c6d46c96c21f29c
nadegeh28 <147403183+nadegeh28@users.noreply.github.com>
2024-12-08 14:20:15 +0100

    créa contact page

---
+++ page-contact.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+<div class="marge">
+
+<h1 class="noustitle">En cas d'urgences</h1>
+
+<div class="container">
+        <div class="nous-container">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/imgurgence.jpg" alt="img">
+        </div>
+        <div class="nous-text">
+            <p>
+            Si vous êtes en danger immédiat ou victime de violences physiques, 
+            il est essentiel d’agir rapidement pour garantir votre sécurité. 
+            Nous comprenons que cela peut être difficile, mais sachez qu’il existe des 
+            ressources et des services disponibles pour vous soutenir, 24h/24 et 7j/7. 
+            </p>
+            <p>
+            N’attendez pas que la situation s’aggrave, 
+            il est crucial de contacter les bons numéros pour obtenir l’aide nécessaire. 
+            Vous méritez d’être protégée et accompagnée dans ce moment difficile. 
+            Ces numéros d’urgence sont là pour vous offrir un soutien immédiat, 
+            vous orienter vers des services adaptés, et vous aider à sortir de la violence. 
+            Vous n’êtes pas seule, de l’aide est à portée de main, et chaque appel peut être un pas 
+            vers votre sécurité et votre guérison
+            </p>
+          
+        </div>
+    </div>
+
+    <h4>En Belgique, voici les numéros d'urgence importants pour les femmes victimes de violences physiques :</h4>
+
+
+</div>
+<?php get_footer(); ?>
