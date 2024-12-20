15418e8ce357cd39a622fc43f7447ad817669c87
nadegeh28 <147403183+nadegeh28@users.noreply.github.com>
2024-12-20 14:31:25 +0100

    ajt bootstrap

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <div class="footer-container">
     <!-- Logo -->
     <div class="footer-logo">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoclair.png" alt="logo">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoclair.png" alt="logo" width="200px">
     </div>
     <!-- Section Help -->
     <div class="footer-help">
