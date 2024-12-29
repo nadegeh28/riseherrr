fc4ac59fdae03050dedc9a531ea3635f372a5594
nadegeh28 <147403183+nadegeh28@users.noreply.github.com>
2024-12-29 16:19:36 +0100

    créa article 1

---
+++ page-article-1.php
@@ -0,0 +1,89 @@
+<?php get_header(); ?>
+
+<div class="marge">
+
+<h4 class="titrecon">Féminicides : la Belgique toujours à la traîne sur la collecte des données</h4>
+
+<div class="nouss-container">
+        <div class="nous-container">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog1.png" alt="blogimg" class="blogimg2">
+        </div>
+        <div class="nous-text">
+            <p>
+            L’Etat belge s’est doté l’été dernier d’un texte pionnier en Europe sur les féminicides, 
+            qui ambitionne de rattraper le retard sur la collecte des données relatives aux violences sexistes. 
+            Mais les associations féministes craignent qu’il soit insuffisant pour réellement endiguer le problème.
+            </p>
+           
+        </div>
+    </div>
+
+    <div class="content">
+    <section>
+
+        <p>
+            Chaque année, en Belgique, des dizaines de femmes sont tuées simplement parce qu’elles sont des femmes.
+            Ces crimes, appelés féminicides, sont l’aboutissement d’un cycle de violences sexistes profondément enracinées 
+            dans notre société. Entre 2012 et 2022, plus de 14.000 femmes ont été tuées en Europe, selon les données d’Eurostat. 
+            Parmi elles, plus de 4.300 ont perdu la vie aux mains de leur conjoint, et 2.400 autres ont été tuées par des membres 
+            de leur famille. En Belgique, cependant, les données officielles sur les victimes d’homicides, y compris leur sexe, 
+            font cruellement défaut.
+        </p>
+        <p>
+        Face à ce manque de statistiques, des initiatives citoyennes et associatives comblent le vide. 
+        Le blog Stop Féminicide recense minutieusement chaque cas rapporté par la presse, 
+        révélant qu’environ 24 à 27 féminicides sont commis chaque année dans le pays, 
+        majoritairement par des conjoints ou ex-conjoints. Ce travail essentiel, réalisé souvent 
+        dans des conditions difficiles, permet de donner un nom et un visage aux victimes.
+        </p>
+
+
+        <p>
+        En 2023, la Belgique a adopté la loi Stop Féminicide, 
+        une avancée historique qui reconnaît officiellement le féminicide comme un crime spécifique et définit 
+        ses différentes formes : féminicide intime (commis par un partenaire ou ex-partenaire), 
+        non intime (par un tiers, comme un client), indirect (résultant de violences comme un avortement forcé), 
+        et homicide fondé sur le genre. Cette loi marque une étape importante en posant les bases pour mieux comprendre 
+        le phénomène grâce à la collecte de statistiques officielles.
+        </p>
+        <p>
+        Malgré cela, de nombreux défis restent à relever. La loi est avant tout symbolique et manque de financements 
+        suffisants pour déployer des mesures concrètes de prévention, comme l’éducation à l’égalité dès le plus jeune âge. 
+        Les associations dénoncent également l’absence d’un véritable ministère des Droits des femmes, capable de coordonner 
+        les politiques à l’échelle nationale. Les féminicides sont l’aboutissement d’un continuum de violences sexistes, 
+        et leur prévention nécessite une remise en question profonde des normes sociétales, notamment celles qui légitiment 
+        encore trop souvent la violence 
+        </p>
+
+    </section>
+
+<h5>Agir pour protéger les victimes</h5>
+        <p>
+        La loi Stop Féminicide prévoit aussi des mesures de protection, comme des interdictions de contact ou de résidence,
+        mais leur mise en œuvre reste compliquée dans la pratique. La création d’un Comité scientifique d’analyse 
+        des féminicides et l’élaboration de rapports statistiques d’ici 2025 sont des avancées prometteuses, 
+        mais leur impact dépendra de la coordination et des moyens alloués.
+        </p>
+
+        <p>
+        Les experts, les associations et les responsables politiques s’accordent sur un point : pour enrayer ce fléau, 
+        la priorité doit être donnée à la prévention. Cela passe par l’éducation, la sensibilisation et 
+        une prise en charge renforcée des victimes par la police et la justice.
+        </p>
+
+
+    <section>
+        
+    <h5>Numéros utiles si vous êtes victime ou témoin de violences sexistes</h5>
+        <p>
+        Si vous ou une personne de votre entourage êtes confrontée à des violences sexistes ou conjugales, plusieurs lignes d’écoute et d’urgence sont disponibles :
+        </p>
+
+        <p>
+        Les experts, les associations et les responsables politiques s’accordent sur un point : pour enrayer ce fléau, 
+        la priorité doit être donnée à la prévention. Cela passe par l’éducation, la sensibilisation et 
+        une prise en charge renforcée des victimes par la police et la justice.
+        </p>
+
+
+    <section>
